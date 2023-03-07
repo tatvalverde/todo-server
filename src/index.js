a6a1a85ea2861d153f05ecb3bfb473567deffc5f
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-03-06 19:32:19 -0500

    return index.js

--- src/index.js
+++ src/index.js
@@ -10,15 +10,18 @@ app.use(home);
 function apiNotFound(req, res) {
   res.send('API not found');
 }
+
 function home(reg,res){
   res.send('Hello, Tanusha!');
 }
+
 function info(reg,res){
   const a = 123;
   const b = Math.random();
   const c = a + b;
   res.send('Info here! ' + c);
 }
+
 app.listen(PORT, ()=> {
   console.log(`Example app listening of http://localhost:${PORT}`);
 });
